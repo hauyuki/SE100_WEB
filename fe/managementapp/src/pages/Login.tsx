0034206acd8d56915b9492ff9b40bcdcc99007a0
hien-le <93437219+hienlephan2003@users.noreply.github.com>
2025-01-02 22:08:22 +0700

    eeee

--- fe/managementapp/src/pages/Login.tsx
+++ fe/managementapp/src/pages/Login.tsx
@@ -3,18 +3,18 @@ import LoginForm from "../components/LoginForm";
 
 const Login = () => {
   return (
-    <div className="h-screen overflow-hidden py-5 w-screen grid grid-cols-3">
-      <div className="flex items-start mx-4 mt-20 justify-center">
+    <div className="h-screen w-screen overflow-hidden grid grid-cols-3">
+      <div className="flex items-center mx-4  justify-center">
         <LoginForm />
       </div>
-      <div className="p-5 col-span-2">
-        <div className=" h-full overflow-hidden">
-          <img
-            alt="banner"
-            src={Images.landingImg}
-            className={`object-cover bg-center overflow-hidden`}
-          ></img>
-        </div>
+      <div className="py-10 px-5 col-span-2 rounded-2xl  w-full h-screen overflow-hidden">
+        {/* <div className=" h-full overflow-hidden"> */}
+        <img
+          alt="banner"
+          src={Images.landingImg}
+          className={`object-cover bg-center rounded-2xl overflow-hidden w-full h-full`}
+        ></img>
+        {/* </div> */}
       </div>
     </div>
   );
